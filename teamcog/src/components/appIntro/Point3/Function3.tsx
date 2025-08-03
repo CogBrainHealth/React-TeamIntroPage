b21afdb407914d13872eec94f7088d9ad761b839
pongdang04 <pongdangdang419@gmail.com>
2025-08-03 14:37:00 +0900

    fix : point3반응형 수정

--- teamcog/src/components/appIntro/Point3/Function3.tsx
+++ teamcog/src/components/appIntro/Point3/Function3.tsx
@@ -1,18 +1,21 @@
 import styles from "./function3.module.css";
 import Image from "next/image";
+
 export default function Function3() {
   return (
     <section className={styles.back}>
-      <p className={styles.title}>*식단 추천</p>
-      <p className={styles.subtitle}>
-        프리미엄 검사 기반으로
-        <br />
-        추천하는 식단 조합
-      </p>
-      <p className={styles.description}>
-        프리미엄 검사 결과를 토대로, <br />
-        나의 상태에 꼭 맞는 맞춤형 식단 조합을 추천합니다.
-      </p>
+      <div className={styles.textContent}>
+        <p className={styles.title}>*식단 추천</p>
+        <p className={styles.subtitle}>
+          프리미엄 검사 기반으로
+          <br />
+          추천하는 식단 조합
+        </p>
+        <p className={styles.description}>
+          프리미엄 검사 결과를 토대로, <br />
+          나의 상태에 꼭 맞는 맞춤형 식단 조합을 추천합니다.
+        </p>
+      </div>
       <Image
         className={styles.image}
         src="/appImages/Point3function3.svg"
