af420d2fc6151e8e2a1e8113a5f17d60ae3c4fb5
pongdang04 <pongdangdang419@gmail.com>
2025-08-03 11:02:36 +0900

    fix/point2 img겹쳐보이는 오류 반응형 수정

--- teamcog/src/components/appIntro/Point2/Point2Description.tsx
+++ teamcog/src/components/appIntro/Point2/Point2Description.tsx
@@ -1,24 +1,29 @@
 import styles from "./description.module.css";
 import Image from "next/image";
+
 export default function Point2Description() {
   return (
     <section className={styles.back}>
-      <Image
-        className={styles.graphImg}
-        src="/appImages/Point2Graph.svg"
-        alt="graph"
-        width={500}
-        height={500}
-        priority
-      />
-      <Image
-        className={styles.games}
-        src="/appImages/Point2Games.png"
-        alt="graph"
-        width={700}
-        height={480}
-        priority
-      />
+      <div className={styles.imageContainer}>
+        <Image
+          className={styles.graphImg}
+          src="/appImages/Point2Graph.svg"
+          alt="graph"
+          width={500}
+          height={500}
+          sizes="(max-width: 768px) 100vw, 50vw"
+          priority
+        />
+        <Image
+          className={styles.games}
+          src="/appImages/Point2Games.png"
+          alt="games"
+          width={700}
+          height={450}
+          sizes="(max-width: 768px) 100vw, 50vw"
+          priority
+        />
+      </div>
     </section>
   );
 }
