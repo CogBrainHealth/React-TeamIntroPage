5fb2bbb8e43b53090e3c9a061791ead11a91b85f
pongdang04 <pongdangdang419@gmail.com>
2025-09-02 12:46:38 +0900

    fix : 이미지최적화 방지

--- teamcog/next.config.ts
+++ teamcog/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/team_cog",
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
